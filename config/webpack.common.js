55f274e0d8494722ba3bcf8493351612fc08222a
rusty1s <matthias.fey@tu-dortmund.de>
2017-07-12 12:39:13 +0200

    outsourced all demo code to demo

--- config/webpack.common.js
+++
@@ -1,5 +0,0 @@
-const common = require('react-dev-config/webpack.common');
-
-common.entry = ['./demo/index.jsx'];
-
-module.exports = common;
--- demo/App.jsx
+++ demo/App.jsx
@@ -1,45 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import {
-  Baseline,
-  Notification,
-  NotificationCenter,
-} from '../src';
+import { Baseline } from '../src';
 
-export default class App extends Component {
-  state = {
-    notifications: [],
-  };
+const App = () => (
+  <Baseline>
+    <div>adwadawd</div>
+    <div>adwadawd</div>
+  </Baseline>
 
-  componentDidMount() {
-    setTimeout(() => this.setState({
-      notifications: [
-        { id: '1', type: 'error', message: 'Hello, world!' },
-        { id: '2', type: 'warning', message: 'Hello, world!' },
-        { id: '3', type: 'success', message: 'Hello, world!' },
-        { id: '4', type: 'info', message: 'Hello, world!' },
-      ],
-    }), 1000);
-  }
+);
 
-  render() {
-    const { notifications } = this.state;
-    return (
-      <Baseline>
-        <NotificationCenter>
-          {notifications.map((n) => {
-            const onClose = () => this.setState({
-              notifications: notifications.filter(a => a.id !== n.id),
-            });
-
-            return (
-              <Notification key={n.id} onClose={onClose} type={n.type}>
-                {n.message}
-              </Notification>
-            );
-          })}
-        </NotificationCenter>
-      </Baseline>
-    );
-  }
-}
+export default App;
